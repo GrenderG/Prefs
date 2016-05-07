8f4fcc24bf3dd3422c095c3e1bec770a87aff18e
GrenderG <grenderg@gmail.com>
2016-05-07 15:30:23 +0200

    First commit

---
+++ lib/src/main/java/es/dmoral/prefs/Prefs.java
@@ -0,0 +1,136 @@
+package es.dmoral.prefs;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by grender on 7/05/16.
+ */
+public class Prefs {
+
+    private static SharedPreferences sharedPreferences;
+    private static Prefs prefsInstance;
+    private static String packageName;
+
+    private static final String PREFERENCES_NAME = "PREFERENCES_" + packageName;
+
+    private static final String DEFAULT_STRING_VALUE = "";
+    private static final int DEFAULT_INT_VALUE = -1;
+    private static final double DEFAULT_DOUBLE_VALUE = -1d;
+    private static final float DEFAULT_FLOAT_VALUE = -1f;
+    private static final long DEFAULT_LONG_VALUE = -1L;
+    private static final boolean DEFAULT_BOOLEAN_VALUE = false;
+
+    private Prefs(@NonNull Context context){
+        if (sharedPreferences == null) {
+            sharedPreferences = context.getApplicationContext().getSharedPreferences(
+                    PREFERENCES_NAME,
+                    Context.MODE_PRIVATE
+            );
+            packageName = context.getApplicationContext().getPackageName();
+        }
+    }
+
+    public static Prefs with(@NonNull Context context) {
+        if (prefsInstance == null) {
+            prefsInstance = new Prefs(context);
+        }
+        return prefsInstance;
+    }
+
+    // String related methods
+
+    public String read(String what) {
+        return sharedPreferences.getString(what, DEFAULT_STRING_VALUE);
+    }
+
+    public String read(String what, String defaultString) {
+        return sharedPreferences.getString(what, defaultString);
+    }
+
+    public void write(String where, String what) {
+        sharedPreferences.edit().putString(where, what).apply();
+    }
+
+    // int related methods
+
+    public int readInt(String what) {
+        return sharedPreferences.getInt(what, DEFAULT_INT_VALUE);
+    }
+
+    public int readInt(String what, int defaultInt) {
+        return sharedPreferences.getInt(what, defaultInt);
+    }
+
+    public void writeInt(String where, int what) {
+        sharedPreferences.edit().putInt(where, what).apply();
+    }
+
+    // double related methods
+
+    public double readDouble(String what) {
+        if (!sharedPreferences.contains(what))
+            return DEFAULT_DOUBLE_VALUE;
+        return Double.longBitsToDouble(readLong(what));
+    }
+
+    public double readDouble(String what, double defaultDouble) {
+        if (!sharedPreferences.contains(what))
+            return defaultDouble;
+        return Double.longBitsToDouble(readLong(what));
+    }
+
+    public void writeDouble(String where, double what) {
+        writeLong(where, Double.doubleToRawLongBits(what));
+    }
+
+    // float related methods
+
+    public float readFloat(String what) {
+        return sharedPreferences.getFloat(what, DEFAULT_FLOAT_VALUE);
+    }
+
+    public float readFloat(String what, float defaultFloat) {
+        return sharedPreferences.getFloat(what, defaultFloat);
+    }
+
+    public void writeFloat(String where, float what) {
+        sharedPreferences.edit().putFloat(where, what).apply();
+    }
+
+    // long related methods
+
+    public long readLong(String what) {
+        return sharedPreferences.getLong(what, DEFAULT_LONG_VALUE);
+    }
+
+    public long readLong(String what, long defaultLong) {
+        return sharedPreferences.getLong(what, defaultLong);
+    }
+
+    public void writeLong(String where, long what) {
+        sharedPreferences.edit().putLong(where, what).apply();
+    }
+
+    // boolean related methods
+
+    public boolean readBoolean(String what) {
+        return sharedPreferences.getBoolean(what, DEFAULT_BOOLEAN_VALUE);
+    }
+
+    public boolean readBoolean(String what, boolean defaultBoolean) {
+        return sharedPreferences.getBoolean(what, defaultBoolean);
+    }
+
+    public void writeBoolean(String where, boolean what) {
+        sharedPreferences.edit().putBoolean(where, what).apply();
+    }
+
+    // end related methods
+
+    public void clear() {
+        sharedPreferences.edit().clear().apply();
+    }
+
+}
